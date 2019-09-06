b2c955ece1e64aa83a67ecb6fe7895a8bc5a2707
Andres Camperi <andrescamperi@Andress-MacBook-Pro.local>
2019-09-06 17:56:34 -0400

    add grommet components

---
+++ links.md
@@ -0,0 +1,33 @@
+*Belle Library* 
+optimized for mobile and desktop
+
+Button:
+http://nikgraf.github.io/belle/#/component/button?_k=a4mtl2
+
+Card (optimized for non-white background though):
+http://nikgraf.github.io/belle/#/component/card?_k=vmx4xg
+
+Dropdown Menu:
+http://nikgraf.github.io/belle/#/component/combo-box?_k=r98uln
+
+Text Input:
+http://nikgraf.github.io/belle/#/component/text-input?_k=2rezol
+
+Toggle: (for when we decide to send the tailored emails):
+http://nikgraf.github.io/belle/#/component/toggle?_k=613pud
+
+*Grommet*
+totally customizable, you can brand it with your own colors
+
+*Halogen*
+spinners for loading 
+https://github.com/yuanyan/halogen
+https://yuanyan.github.io/halogen/
+
+*Joyride*
+for guided tours of the app, maybe useful when we have a dashboard
+https://github.com/gilbarbara/react-joyride
+
+*PrimeReact*
+complete UI framework
+https://www.primefaces.org/primereact/#/
---
+++ src/theme.js
@@ -0,0 +1,73 @@
+export const theme = {
+  "name": "adsf",
+  "rounding": 14,
+  "spacing": 24,
+  "global": {
+    "colors": {
+      "brand": "#C0EC46",
+    },
+    "font": {
+      "family": "Times New Roman",
+      "size": "18px",
+      "height": "24px",
+      "maxWidth": "432px"
+    },
+    "control": {
+      "border": {
+        "radius": "14px"
+      }
+    },
+    "borderSize": {
+      "xsmall": "1px",
+      "small": "2px",
+      "medium": "4px",
+      "large": "12px",
+      "xlarge": "24px"
+    },
+    "input": {
+      "padding": "12px",
+      "weight": 600
+    },
+    "spacing": "24px",
+    "size": {
+      "xxsmall": "48px",
+      "xsmall": "96px",
+      "small": "192px",
+      "medium": "384px",
+      "large": "768px",
+      "xlarge": "1152px",
+      "xxlarge": "1536px",
+      "full": "100%"
+    },
+    "focus": {
+      "border": {
+        "color": "#C0EC46"
+      }
+    }
+  },
+  "button": {
+    "border": {
+      "width": "2px",
+      "radius": "8px"
+    },
+    "padding": {
+      "vertical": "4px",
+      "horizontal": "22px"
+    }
+  },
+  "checkBox": {
+    "check": {
+      "radius": "6px"
+    },
+    "toggle": {
+      "radius": "24px",
+      "size": "48px"
+    },
+    "size": "24px"
+  },
+  "radioButton": {
+    "size": "24px"
+  },
+}
+
+export default theme;
